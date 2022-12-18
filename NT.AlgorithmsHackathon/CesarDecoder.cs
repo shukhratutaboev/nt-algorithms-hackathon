aa3b708c59d5f1b51521a9c6907d2b92dfd7e2de
shukhratutaboev <shuhratutaboyev2597@gmail.com>
2022-12-19 00:27:03 +0500

    somechanges

--- NT.AlgorithmsHackathon/CesarDecoder.cs
+++ NT.AlgorithmsHackathon/CesarDecoder.cs
@@ -2,8 +2,8 @@ namespace NT.AlgorithmsHackathon;
 
 public class CesarDecoder
 {
-    private readonly char[] _upper = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    private readonly char[] _lower = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    private readonly char[] _uppers = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    private readonly char[] _lowers = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     private readonly char[] _numbers = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     public string FromCesar(string txt)
@@ -12,13 +12,13 @@ public class CesarDecoder
 
         foreach(var c in txt)
         {
-            if(_upper.Contains(c))
+            if(_uppers.Contains(c))
             {
-                plainText += _upper[(Array.IndexOf(_upper, c) + 9) % 26];
+                plainText += _uppers[(Array.IndexOf(_uppers, c) + 9) % 26];
             }
-            else if(_lower.Contains(c))
+            else if(_lowers.Contains(c))
             {
-                plainText += _lower[(Array.IndexOf(_lower, c) + 9) % 26];
+                plainText += _lowers[(Array.IndexOf(_lowers, c) + 9) % 26];
             }
             else if(_numbers.Contains(c))
             {
