2e957fbbaa001a4900e1ac812e1c1e98deafdbce
shukhratutaboev <shuhratutaboyev2597@gmail.com>
2022-12-19 00:35:03 +0500

    somechanges

--- NT.AlgorithmsHackathon/Program.cs
+++ NT.AlgorithmsHackathon/Program.cs
@@ -1,33 +1,33 @@
 ﻿using MySql.Data.MySqlClient;
 
-string connStr = "connectionString";
+string connectionString = "connectionString";
 
-MySqlConnection conn = new MySqlConnection(connStr);
+MySqlConnection connection = new MySqlConnection(connectionString);
 try
 {
     Console.WriteLine("Connecting to MySQL...");
-    conn.Open();
+    connection.Open();
     
-    string sql = @"SELECT e.emp_no, e.first_name, e.last_name, SUM(salary), MIN(s.from_date), MAX(s.to_date), eh.end_date FROM salaries as s 
-    JOIN (SELECT emp_no, MAX(eh.to_date) as end_date FROM dept_emp AS eh GROUP BY eh.emp_no) as eh ON eh.emp_no = s.emp_no
+    string sql = @"SELECT e.emp_no, e.first_name, e.last_name, SUM(salary), MIN(s.from_date), MAX(s.to_date), eh.end_date FROM salaries AS s 
+    JOIN (SELECT emp_no, MAX(eh.to_date) AS end_date FROM dept_emp AS eh GROUP BY eh.emp_no) AS eh ON eh.emp_no = s.emp_no
     JOIN employees as e ON e.emp_no = s.emp_no   
     WHERE eh.end_date < s.to_date
     GROUP BY e.emp_no;";
-    MySqlCommand cmd = new MySqlCommand(sql, conn);
-    MySqlDataReader rdr = cmd.ExecuteReader();
+    MySqlCommand cmd = new MySqlCommand(sql, connection);
+    MySqlDataReader reader = cmd.ExecuteReader();
 
     Console.WriteLine("Employee fullname -- Salary -- From date -- To date   -- End date");
-    while (rdr.Read())
+    while (reader.Read())
     {
-        Console.WriteLine(rdr[1] + " " + rdr[2] + "     -- " + rdr[3] + " -- " + rdr[4].ToString().Split()[0] + " -- " + rdr[5].ToString().Split()[0] + " -- " + rdr[6].ToString().Split()[0]);
+        Console.WriteLine(reader[1] + " " + reader[2] + "     -- " + reader[3] + " -- " + reader[4].ToString().Split()[0] + " -- " + reader[5].ToString().Split()[0] + " -- " + reader[6].ToString().Split()[0]);
     }
-    rdr.Close();
+    reader.Close();
 }
 catch (Exception err)
 {
     Console.WriteLine(err.ToString());
 }
 
-conn.Close();
+connection.Close();
 Console.WriteLine("Connection Closed. Press any key to exit...");
 Console.Read();
